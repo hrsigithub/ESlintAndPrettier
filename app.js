e66434fc3131bd61f427fe614682ec938982d666
hrsi <hrsi.ad@gmail.com>
2023-11-11 17:44:00 +0900

    初期コミット

---
+++ app.js
@@ -0,0 +1,21 @@
+const a = 'AAA';
+
+const b = 1;
+
+// eslint-disable-next-line no-console
+console.log(a);
+
+console.warn(b);
+
+console.warn('Log a warn level message.');
+console.error('Log an error level message.');
+
+const helloWorld = 'aa'; // camal
+const hello_World = 'aa';
+
+console.warn(helloWorld);
+console.warn(hello_World);
+
+if (b === 2) {
+  let a = 1;
+}
