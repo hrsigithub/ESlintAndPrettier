b4b3f5e9f5e157580914cd6586c920802f26c55f
hrsi <hrsi.ad@gmail.com>
2023-11-11 18:35:07 +0900

    test2 commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const a = 'AAA';
 
 const b = 1;
+const c = 2;
 
 // eslint-disable-next-line no-console
 console.log(a);
 
 console.warn(b);
+console.log(c);
 
 console.warn('Log a warn level message.');
 console.error('Log an error level message.');
